b3f9ee15d3345aa64fd058b9708c8776813db8b2
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-30 10:03:17 -0700

    added babel/node to dev dependencies

---
+++ src/play.js
@@ -0,0 +1,30 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+
+const logger = store => next => action => {
+  /* eslint-disable-next-line no-console */
+  console.log(action);
+  next(action);
+  console.log(action);
+  next(action);
+  console.log(action);
+  next(action);
+};
+
+function reducer(state = {}, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+const window = window || {};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore (
+  reducer, 
+  composeEnhancers(applyMiddleware(logger)),
+);
+
+store.dispatch({ type: 'LIKE CHEECH AND CHONG' });
+
+store.dispatch({ type: 'HITS FROM THE BONG' });
+
