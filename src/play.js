f352a0ac8c3ed717e20d60aad6cf801ffe1015fa
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-30 22:35:41 -0700

    completely refactored this for rates api not yet working though

--- src/play.js
+++ src/play.js
@@ -1,21 +1,57 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+// import Thunk from 'redux-thunk';  master ryan doesn't have this line in his code
+// so it probably isn't needed
+import getRates from './services/ratesApi';
 
 const logger = store => next => action => {
+
+  const oldState = store.getState();
   /* eslint-disable-next-line no-console */
   console.log(action);
   next(action);
-  console.log(action);
-  next(action);
-  console.log(action);
-  next(action);
+
+  const newState = store.getState();
+
+  /* eslint-disable-next-line no-console */
+  if(oldState !== newState) console.log('updated', newState);
+};
+
+const thunk = store => next => action => {
+  if(typeof action === 'function') {
+    action(store.dispatch);
+  }
+  else {
+    next(action);
+  }
+};
+
+const promiseMiddleware = store => next => action => {
+  if(Promise.resolve(action.payload) === action.payload) {
+    next({
+      type: 'PENDING',
+    });
+    action.payload
+      .then(data => {
+        store.dispatch({
+          type: action.type,
+          payload: data,
+        });
+      });
+  }
+  else {
+    next(action);
+  }
 };
 
 function reducer(state = {}, action) {
   switch(action.type) {
+    case 'FETCH_RATES':
+      return action.payload;
     default:
       return state;
   }
 }
+
 const window = window || {};
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -24,11 +60,32 @@ const store = createStore (
   composeEnhancers(applyMiddleware(logger)),
 );
 
+// const fetchRates = () => ({
+//   type: 'FETCH_RATES',
+//   payload: getRates()
+// });
+
 store.dispatch({ type: 'LIKE CHEECH AND CHONG' });
 
 store.dispatch({ type: 'HITS FROM THE BONG' });
 
+// I think this is a different way to do the fetch thing, will play around with later
+// const fetchRates = () => dispatch => {
+//   fetch('https://api.ratesapi.io/api/latest?base=USD')
+//     .then(res => res.json)
+// }
+
+// fetchRates is an action creator
+// it returns a new style action (because the action is a function)
 const fetchRates = () => dispatch => {
   fetch('https://api.ratesapi.io/api/latest?base=USD')
-    .then(res => res.json)
-}
+    .then(res => res.json())
+    .then(rates => {
+      dispatch({
+        type: 'FETCH_RATES',
+        payload: rates
+      });
+    });
+};
+
+store.dispatch(fetchRates());
